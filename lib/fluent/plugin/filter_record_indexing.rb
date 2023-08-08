872579bc7749fe084f6cd479c556461388a9e634
imcotop <141424264+imcotop@users.noreply.github.com>
2023-08-08 16:30:52 +0300

    Update filter_record_indexing.rb

--- lib/fluent/plugin/filter_record_indexing.rb
+++ lib/fluent/plugin/filter_record_indexing.rb
@@ -1,31 +1,36 @@
 require "fluent/filter"
 
 module Fluent
-    class RecordIndexingFilter < Filter
-      Fluent::Plugin.register_filter("record_indexing", self)
+  class RecordIndexingFilter < Filter
+    Fluent::Plugin.register_filter("record_indexing", self)
+    
+    desc "Key name to spin"
+    config_param :key_name, :string, default: nil
+    config_param :key_prefix, :string, default: nil
+    config_param :check_all_key, :bool, default: true
 
-      desc "Key name to spin"
-      config_param :key_name, :string, default: nil
-      config_param :key_prefix, :string, default: nil
-      config_param :check_all, :bool, default: true
-
-      def transform(tag,record)
+    def filter(tag, time, record)
+      if check_all_key == false
         unless key_name
           raise ArgumentError, "key_name parameter is required"
         end
-        new_record = {}
-        record.each do |key, value|
-          if check_all || (key == key_name && value.is_a?(Array))
-            new_record[key] = {}
+      end
+      new_record = {}
+      record.each do |key, value|
+        if check_all_key || key == key_name
+          new_record[key] = {}
+          if value.is_a?(Array)
             value.each_with_index do |item, index|
               new_record[key]["#{key_prefix}#{index}"] = item
             end
           else
             new_record[key] = value
           end
+        else
+          new_record[key] = value
         end
-        new_record
       end
+      new_record
     end
   end
 end
